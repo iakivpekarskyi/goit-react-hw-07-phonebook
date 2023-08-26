5c6cfd99f6901b91548dddc6d60422ccb364d679
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-08-26 18:12:31 +0200

    doc(store) delete persistor

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import { App } from 'components/App/App';
 import 'normalize.css';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { persistor, store } from './redux/store';
+import { store } from 'redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </BrowserRouter>
 );
